c091552033e98ad02570cd7c435f39b020c223ce
Aldrin Cenina <amnacenina16@gmail.com>
2026-03-04 23:52:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1039,4 +1039,5 @@ load_binary_model()
 print("="*60)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
